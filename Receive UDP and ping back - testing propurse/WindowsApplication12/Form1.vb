82be20b6e394579a89b282d61d67595384135289
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:21:37 -0300

    old project migrated

---
+++ Receive UDP and ping back - testing propurse/WindowsApplication12/Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+
+Public Class Form1
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim U As New UdpClient(65000)
+
+        While True
+            Dim Message As UdpReceiveResult = Await U.ReceiveAsync()
+
+            Dim r As Byte() = System.Text.Encoding.UTF8.GetBytes(Message.RemoteEndPoint.ToString)
+            Await U.SendAsync(r, r.Length, Message.RemoteEndPoint)
+        End While
+    End Sub
+
+End Class
---
+++ Send data to raptor via udp and wait ping back/WindowsApplication34/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+
+Public Class Form1
+
+    Dim U As New UdpClient(65000)
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        U.Send(System.Text.Encoding.UTF8.GetBytes("abc"), 3, New IPEndPoint(IPAddress.Parse("54.88.128.121"), 65000))
+    End Sub
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim RTask As Task(Of UdpReceiveResult) = U.ReceiveAsync
+        U.Send(System.Text.Encoding.UTF8.GetBytes("abc"), 3, New IPEndPoint(IPAddress.Parse("54.88.128.121"), 65000))
+
+
+        Dim back As UdpReceiveResult = Await RTask
+        MsgBox(System.Text.Encoding.UTF8.GetString(back.Buffer))
+
+    End Sub
+
+    Private Async Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim RTask As Task(Of UdpReceiveResult) = U.ReceiveAsync
+        Dim back As UdpReceiveResult = Await RTask
+        MsgBox(System.Text.Encoding.UTF8.GetString(back.Buffer))
+
+    End Sub
+
+End Class
